9ec50adfd1ceb586f91987307db0c036189a2d43
Julesntizimira <ntizimijules5@gmail.com>
2024-01-11 12:12:45 +0200

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''define function to_str()
+'''
+
+
+def to_str(n: float) -> str:
+    """
+    convert float to str
+
+    Parameters:
+    - n (float): The float argument.
+
+    Returns:
+    str: The string represantation of the float.
+    """
+    return str(n)
