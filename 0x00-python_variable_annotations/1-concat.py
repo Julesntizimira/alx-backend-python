02290c91b7b4c65f7aa5a0bd0db639a07e3c9a87
Julesntizimira <ntizimijules5@gmail.com>
2024-01-11 11:47:18 +0200

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''define function concat()
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concatenate two strings.
+
+    Parameters:
+    - str1 (str): The first string.
+    - str2 (str): The second string.
+
+    Returns:
+    float: The concatenated string.
+    """
+    return str1 + str2
