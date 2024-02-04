07d4772bc5b9ca08a566fa2789c69a01b9a8532f
Julesntizimira <ntizimijules5@gmail.com>
2024-02-04 23:09:36 +0200

    More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -24,3 +24,15 @@ class TestGithubOrgClient(unittest.TestCase):
         with patch.object(GithubOrgClient, 'org', mocked_property):
             myclass = GithubOrgClient("args")
             self.assertEqual(myclass._public_repos_url, "mocked property")
+    
+    @patch('client.get_json')
+    def test_public_repos(self, mocked_get_json):
+        '''test public_repos
+        '''
+        with patch.object(GithubOrgClient, "_public_repos_url", PropertyMock()) as mocked_property:
+            mocked_property.return_value = [{"name": "jules", "licence": "43567"}]
+            mocked_get_json.return_value = [{"name": "jules", "licence": "43567"}]
+            myclass = GithubOrgClient("args")
+            self.assertEqual(myclass.public_repos(), ["jules"])
+            mocked_property.assert_called_once()
+            mocked_get_json.assert_called_once()
